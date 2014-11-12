70af583054ce6e8166af60238a5b7c0292764d08
carolina.dalfonso@gmail.com <carolina.dalfonso@gmail.com@5a78bd0e-b2c4-ac9e-5342-61b1dbe6d5df>
2014-11-12 01:51:06 +0000

    Subo version - Caro

---
+++ FrbaHotel/ClasesTabla/STipoDoc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrbaHotel.ClasesTabla
+{
+    class STipoDoc
+    {
+        private DTipoDoc _tipoDao;
+        private DTipoDoc tipoDao
+        {
+            get
+            {
+                if (_tipoDao == null)
+                    _tipoDao = new DTipoDoc();
+                return _tipoDao;
+            }
+        }
+
+        public List<TipoDoc> GetAll()
+        {
+            return tipoDao.GetAll();
+        }
+
+        public List<TipoDoc> GetBySQL(string consulta)
+        {
+            return tipoDao.GetBySQL(consulta);
+        }
+
+    }
+}
--- FrbaHotel/Program.cs
+++ FrbaHotel/Program.cs
@@ -15,7 +15,7 @@ namespace FrbaHotel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Inicial());
+            Application.Run(new FormIni());
         }
     }
 }
